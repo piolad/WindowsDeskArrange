91e4cf4eb71deac86239120c5bc9b27f8276c730
Piotr Ladkowski <piolad2003@gmail.com>
2024-07-31 20:29:10 +0200

    WIP move towards Classes and inheritence.

--- ViewCardBase.py
+++ ViewCardBase.py
@@ -16,18 +16,21 @@ class ViewCardBase(customtkinter.CTkFrame):
                  background_corner_colors: Union[Tuple[Union[str, Tuple[str, str]]], None] = None,
                  overwrite_preferred_drawing_method: Union[str, None] = None,
                  **kwargs):
-        super().__init__(master=master, bg_color=bg_color, width=width, height=height, **kwargs)
+        super().__init__(master, width=width, height=height, corner_radius=corner_radius, border_width=border_width,
+                            bg_color=bg_color, fg_color=fg_color, border_color=border_color,
+                            background_corner_colors=background_corner_colors,
+                            overwrite_preferred_drawing_method=overwrite_preferred_drawing_method, **kwargs)
         
         # example: a label and a button
         
         
 
-        self.label = customtkinter.CTkLabel(self, text="ViewCardBase", font=customtkinter.CTkFont(size=10, weight="bold"))
-        self.label.pack()
-        self.button = customtkinter.CTkButton(self, text="ViewCardBase Button", command=self.button_event)
+        # self.label = customtkinter.CTkLabel(self, text="ViewCardBase", font=customtkinter.CTkFont(size=10, weight="bold"))
+        # self.label.pack()
+        # self.button = customtkinter.CTkButton(self, text="ViewCardBase Button", command=self.button_event)
         # self.button.grid(row=1, column=0)
-        self.button.pack()
-        self.pack()
+        # self.button.pack()
+        # self.pack()
     
     def button_event(self):
         pass
