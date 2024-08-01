96b09184492b617888f7d2e889b925ffa309d3bc
Piotr Ladkowski <piolad2003@gmail.com>
2024-08-01 18:58:34 +0200

    add a basis of RegFunctions.py

---
+++ RegFunctions.py
@@ -0,0 +1,8 @@
+
+baseRegPath = r'\HKEY_CURRENT_USER\Software\Microsoft\Windows\Shell\Bags\1\Desktop'
+
+def loadCurrentDesktopReg():
+    """
+    Load the current desktop registry settings and return them for backup as binary or text file.
+    """
+    pass
